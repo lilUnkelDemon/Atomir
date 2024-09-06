42872a718cfcef772596311a1797c966cdda0782
lilUnkelDemon <amir.momeni.gelloria79@gmail.com>
2024-09-06 21:40:10 +0330

    update commands file

--- commands.php
+++ commands.php
@@ -1,5 +1,6 @@
 <?php
 
+// کدهای ANSI برای رنگ‌ها
 $colors = [
     'green' => "\033[32m",
     'yellow' => "\033[33m",
@@ -7,27 +8,31 @@ $colors = [
     'reset' => "\033[0m"
 ];
 
-$options = getopt("c:p:");
+// دریافت آرگومان‌های خط فرمان
+$command = isset($argv[1]) ? $argv[1] : null;
+$port = 8585; // پورت پیش‌فرض
 
-$command = isset($options['c']) ? $options['c'] : null;
-$port = isset($options['p']) ? $options['p'] : 8585;
+// بررسی اینکه آیا آرگومان پورت دریافت شده یا خیر
+if (isset($argv[2]) && $argv[2] == '-p' && isset($argv[3])) {
+    $port = $argv[3];
+}
 
 switch ($command) {
     case 'run':
-        echo  "[{$colors['green']}Running{$colors['reset']}] on port " . $port . "\n" ;
+        echo "[{$colors['green']}Running{$colors['reset']}] on port " . $port . "\n";
         exec("php -S localhost:" . $port . " -t public");
         break;
 
     case 'stop':
-        echo "[{$colors['red']}Stopping{$colors['reset']}] the server (This is just an example, implement actual logic).\n" ;
+        echo "[{$colors['red']}Stopping{$colors['reset']}] the server (This is just an example, implement actual logic).\n";
         break;
 
     case 'restart':
-        echo "[{$colors['yellow']}Restarting{$colors['reset']}] the server on port " . $port . "\n"  ;
+        echo "[{$colors['yellow']}Restarting{$colors['reset']}] the server on port " . $port . "\n";
         exec("php -S localhost:" . $port . " -t public");
         break;
 
     default:
-        echo $colors['red'] . "Unknown command.\n" . $colors['reset'];
+        echo $colors['red'] . "Unknown command. Use 'run', 'stop', or 'restart'.\n" . $colors['reset'];
         break;
 }
---
+++ storage/cache/views/aaf386663cf34161bfa18bea2e62f63396bc6a0e.php
@@ -0,0 +1,6 @@
+<?php $__env->startSection('content'); ?>
+
+    <h1 class="text-2xl font-bold bg-green-500">Welcome to our About!</h1>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/herrteufel/Projeketen/meinproject/test/Atomir/resources/views/about.blade.php ENDPATH**/ ?>
